80749002845d82dfdd41e1de501e1c0b54458043
unknown <anitapandey9575@gmail.com>
2026-04-14 15:47:57 +0530

    Clean push5

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,16 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
-
-  
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"           
-    storage_account_name = "anitaterraformstatestore" 
-    container_name       = "tfstate"               
-    key                  = "terraform.tfstate"     
-  }
 }
 
 provider "azurerm" {
-  features {} 
+  features {
+    resource_group {
+     
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
